c1e8b47e22df656133922668e3435274ecced83b
fc <francois.corbel@gmail.com>
2021-04-26 21:59:53 +0200

    Add quit on escape

---
+++ Assets/Scripts/Quit.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Quit : MonoBehaviour {
+#if UNITY_WEBGL
+#else
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Application.Quit();
+        }
+    }
+#endif
+}
